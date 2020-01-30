011a24f8563c1bea5503680b04af4e88153b7ed9
Dylan Kreth <dylankreth@gmail.com>
2020-01-29 23:51:54 -0600

    test path

--- src/main/java/com/dak/urlshortener/RecordController.java
+++ src/main/java/com/dak/urlshortener/RecordController.java
@@ -6,6 +6,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RecordController {
+
+    HashMap<String, String> map = new HashMap<>(); //should this go in UrlShortenerApplication? What goes there?
+
+    @GetMapping("test")
+    public Record testPath(){
+        return new Record("test","test");
+    }
+
     @GetMapping("/generate")
     public Record generate(@RequestParam(name = "longUrl") String longUrl , @RequestParam(name = "shortId") String shortId) {
         return new Record(shortId,longUrl);
